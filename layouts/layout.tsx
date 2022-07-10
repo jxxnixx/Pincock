e18591d3e8fc6c2f5409d59cdd550612bc2b75cc
shubug1015 <shubug@naver.com>
2022-07-10 18:37:22 +0900

    초기 tailwind, layout 설정

---
+++ layouts/layout.tsx
@@ -0,0 +1,7 @@
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: IProps) {
+  return <div className='mx-auto w-full max-w-[330px]'>{children}</div>;
+}
